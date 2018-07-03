964182b87a51494209b2d0da4a15a6289ad9064c
Eche's Macbook <macbookpro@Eches-MacBook-Pro.local>
2018-07-03 10:27:48 +0100

    1st lesson

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Eche's Macbook  on 03/07/2018.
+//  Copyright © 2018 Eche's Macbook . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        swoosh.frame = CGRect
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
